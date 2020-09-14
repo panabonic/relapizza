5e0b5bd93056f75020d99600c3d075a27b0c8188
panabonic <panabonic@gmail.com>
2020-09-14 16:00:57 +0300

    create products component

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>RelaPizza</title>
+
+</head>
+<body class="antialiased">
+    <div id="app"></div>
+</body>
+<script type="text/javascript" src="/js/index.js"></script>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.react('resources/js/index.js', 'public/js')
+    .copy('resources/css/index.css', 'public/css');
