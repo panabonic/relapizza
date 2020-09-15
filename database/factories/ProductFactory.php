290d9c3923784e4380bf1a6c315aa6b14995e655
panabonic <panabonic@gmail.com>
2020-09-15 17:38:30 +0300

    create products model and controller

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(3, true),
+            'description' => $this->faker->text(),
+            'image' => $this->faker->imageUrl(510, 650, 'food'),
+            'price' => $this->faker->randomFloat(2, 5, 30)
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::delete('/products/{product}', [ProductController::class, 'delete']);
+
+Route::post('/products', [ProductController::class, 'store']);
